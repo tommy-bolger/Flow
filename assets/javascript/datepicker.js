c3429726c9ccede3ee32dd1bfd6d610b8e4ac285
Tommy Bolger <tommy.bolger@gmail.com>
2011-12-30 13:06:07 -0800

    Removed deleted files.

--- assets/javascript/datepicker.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	$(".date_field").datepicker({
-		changeMonth: true,
-		changeYear: true,
-		showButtonPanel: true
-	});
-});
--- index.php
+++
@@ -1,8 +0,0 @@
-<?php  
-//The index file that calls the framework
-
-//Include the framework file
-require_once("./framework/core/Framework.class.php");
-
-//Execute SquFramework
-Framework::runWeb("./framework/core/configuration.ini");
