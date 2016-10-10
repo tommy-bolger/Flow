2709de90c9b9e85f9f889b8f0642d221d8781d71
Tommy <tommy.bolger@gmail.com>
2016-10-10 09:25:16 -0700

    Added a file to configure server connections for use with the cache library.

---
+++ protected/cache_connections.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+    'default' => array(
+        'module' => 'redis',
+        'options' => array(
+            'host' => '127.0.0.1',
+            'port' => 6379
+        )
+    )
+);
